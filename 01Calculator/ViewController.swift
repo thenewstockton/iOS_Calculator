ce3d31901386906d36dc87111cf825e89d2a3dd7
Phil <phillip5914619@gmail.com>
2017-03-31 23:40:44 +0800

    Lecture 1  simple calculator

--- 01Calculator/ViewController.swift
+++ 01Calculator/ViewController.swift
@@ -1,25 +1,45 @@
-//
-//  ViewController.swift
-//  01Calculator
-//
-//  Created by Ko on 3/31/17.
-//  Copyright © 2017 thefreshstockton. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+ 
+	@IBOutlet weak var display: UILabel!
+	
+	var userIsInTheMiddleOfTyping = false
+	
+	@IBAction func ButtonTapped(_ sender: UIButton) {
+		let digit = sender.currentTitle!
+		
+		if (userIsInTheMiddleOfTyping){
+			let textCurrentlyInDisplay = display.text!
+			display.text = textCurrentlyInDisplay + digit
+		} else {
+			display.text = digit
+			userIsInTheMiddleOfTyping = true
+		}
+	}
+	
+	var displayValue: Double {
+		get{
+			return Double(display.text!)!
+		}
+		set{
+			display.text = String(newValue)
+		}
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	} 
-
+	@IBAction func performPiOperation(_ sender: UIButton) {
+		userIsInTheMiddleOfTyping = false
+		if let mathematicalSymbol = sender.currentTitle {
+			switch mathematicalSymbol{
+				case "π":
+					displayValue = Double.pi
+				case "√":
+					displayValue = sqrt(displayValue)
+				default:
+					break;
+			}
+		}
+	}
+	
 
 }
-
